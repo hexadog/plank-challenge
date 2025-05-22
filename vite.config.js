a3e19ae82adb4ef84547419ff6d00189ee2b82de
Gaetan <72258504+gaetan-hexadog@users.noreply.github.com>
2025-05-22 11:34:30 +0200

    fix

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import { defineConfig } from 'vite';
 import { resolve } from 'path';
 
 export default defineConfig({
+    base: './', // Préfixe toutes les URLs des assets avec ./
     build: {
         outDir: 'dist',
         rollupOptions: {
